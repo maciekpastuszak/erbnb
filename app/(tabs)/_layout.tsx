fb7fb4633eef106c86ac4d063e3013336d15b87d
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-06 10:25:08 +0100

    update primary color in Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import Colors from '../../constants/Colors'
 
 const Layout = () => {
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: Colors.primary,
+    }}>
       <Tabs.Screen 
         name="index" 
         options={{
