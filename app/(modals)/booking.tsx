b04655cbd14b04f0d544e3a7946347a49a7696a7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-01-07 20:54:38 +0100

    add links in booking page

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const Page = () => {
   return (
     <View>
-      <Text>Booking</Text>
+      <Link href={'/(modals)/login'}>
+        Login
+      </Link>
+      <Link href={'/(modals)/booking'}>
+        Bookings
+      </Link>
     </View>
   )
 }
